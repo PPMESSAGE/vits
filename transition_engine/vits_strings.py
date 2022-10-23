f80d14dc528e3693c6a4b7c83d26659be6e7fd60
Guijin Ding <dingguijin@gmail.com>
2022-10-23 15:52:14 +0800

    test baker

--- transition_engine/vits_strings.py
+++ transition_engine/vits_strings.py
@@ -13,6 +13,7 @@ from merge_text import convert_pinyin
 
 sys.path.append('..')
 import utils
+import commons
 from models import SynthesizerTrn
 
 from text import pinyin_symbols
@@ -29,6 +30,7 @@ def save_wav(wav, path, rate):
 
 def get_text_ids(phones, hps):
     text_norm = cleaned_text_to_sequence(phones)
+    text_norm = commons.intersperse(text_norm, 0)
     text_norm = torch.LongTensor(text_norm)
     return text_norm
 
@@ -37,50 +39,35 @@ def get_text_ids(phones, hps):
 hps = utils.get_hparams_from_file("../configs/tr_base.json")
 
 net_g = SynthesizerTrn(
-    len(symbols),
+    len(pinyin_symbols),
     hps.data.filter_length // 2 + 1,
     hps.train.segment_size // hps.data.hop_length,
-    **hps.model).cuda()
+    **hps.model)
 _ = net_g.eval()
 
-_ = utils.load_checkpoint("../logs/tr_base/G_160000.pth", net_g, None)
-
-# check directory existence
-if not os.path.exists("./vits_out"):
-    os.makedirs("./vits_out")
+_ = utils.load_checkpoint("../logs/tr_base/latest.pth", net_g, None)
 
 if __name__ == "__main__":
-    n = 0
-    fo = open("vits_strings.txt", "r+")
-    while(True):
-        try:
-            message = fo.readline().strip()
-        except Exception as e:
-            print('nothing of except:', e)
-            break
-        if (message == None):
-            break
-        if (message == ""):
-            break
-        n = n + 1
+    print("===============================================================")
+    phonemes = chinese_to_phonemes(sys.argv[1])
+    input_ids = get_text_ids(phonemes, hps)
 
-        print("===============================================================")
-        phonemes = chinese_to_phonemes(message)
-        input_ids = get_text_ids(phonemes, hps)
+    print(datetime.datetime.now())
+    with torch.no_grad():
+        x_tst = input_ids.unsqueeze(0)
+        x_tst_lengths = torch.LongTensor([input_ids.size(0)])
+        #y_hat, attn, mask, *_ = generator.module.infer(x, x_lengths, max_len=1000)
+        print(x_tst)
+        print(x_tst_lengths)
+        y_hat, attn, mask, *_ = net_g.infer(x_tst, x_tst_lengths, max_len=1000)
+        y_hat = y_hat.cpu()
+        y_hat_lengths = mask.sum([1,2]).long() * hps.data.hop_length
+    print(datetime.datetime.now())
 
-        print(datetime.datetime.now())
-        with torch.no_grad():
-            x_tst = input_ids.cuda().unsqueeze(0)
-            x_tst_lengths = torch.LongTensor([input_ids.size(0)]).cuda()
-            audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=0, noise_scale_w=0, length_scale=1)[0][0,0].data.cpu().float().numpy()
-        print(datetime.datetime.now())
+    print("hat_length", y_hat_lengths)
+    audio = y_hat[0,:,:y_hat_lengths[0]].numpy()[0]
+    print("audio", audio)
+    save_wav(audio, "./vits_out/baker.wav", hps.data.sampling_rate)
 
-        save_wav(audio, f"./vits_out/{n}_baker.wav", hps.data.sampling_rate)
-
-        print(message)
-        print(phonemes)
-        print(input_ids)
-    fo.close()
-
-    # can be deleted
-    os.system("chmod 777 ./vits_out -R")
+    print(phonemes)
+    print(input_ids)
